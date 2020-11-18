9d79f1da00080df0af436cb5fe03a2aac434cf7f
thedualspace <64597895+thedualspace@users.noreply.github.com>
2020-11-18 20:09:08 +0000

    First commit

---
+++ CoPerspectiv Website 2/app.js
@@ -0,0 +1,147 @@
+function init() {
+  const slides = document.querySelectorAll(".slide");
+  const pages = document.querySelectorAll(".page");
+  const backgrounds = [
+    `linear-gradient(#00B5BB, #1C5C5F)`,
+    `linear-gradient(#529eed, #334cb2)`,
+    `linear-gradient(#2c57d4, #333399)`,
+    `linear-gradient(#ffffff, #000000)`,
+  ];
+  //Tracker
+  let current = 0;
+  let scrollSlide = 0;
+
+  slides.forEach((slide, index) => {
+    slide.addEventListener("click", function() {
+      changeDots(this);
+      nextSlide(index);
+      scrollSlide = index;
+    });
+  });
+
+  function changeDots(dot) {
+    slides.forEach(slide => {
+      slide.classList.remove("active");
+    });
+    dot.classList.add("active");
+  }
+
+  function nextSlide(pageNumber) {
+    const nextPage = pages[pageNumber];
+    const currentPage = pages[current];
+    const nextLeft = nextPage.querySelector(".hero .model-left");
+    const nextRight = nextPage.querySelector(".hero .model-right");
+    const currentLeft = currentPage.querySelector(".hero .model-left");
+    const currentRight = currentPage.querySelector(".hero .model-right");
+    const nextText = nextPage.querySelector(".details");
+    const portofolio = document.querySelector(".portofolio");
+
+    const tl = new TimelineMax({
+      onStart: function() {
+        slides.forEach(slide => {
+          slide.style.pointerEvents = "none";
+        });
+      },
+      onComplete: function() {
+        slides.forEach(slide => {
+          slide.style.pointerEvents = "all";
+        });
+      }
+    });
+
+    tl.fromTo(currentLeft, 0.3, { y: "-10%" }, { y: "-100%" })
+      .fromTo(currentRight, 0.3, { y: "10%" }, { y: "-100%" }, "-=0.2")
+      .to(portofolio, 0.3, { backgroundImage: backgrounds[pageNumber] })
+      .fromTo(
+        currentPage,
+        0.4,
+        { opacity: 1, pointerEvents: "all" },
+        { opacity: 0, pointerEvents: "none" }
+      )
+      .fromTo(
+        nextPage,
+        0.3,
+        { opacity: 0, pointerEvents: "none" }, 
+        { opacity: 1, pointerEvents: "all" }, 
+        "-=0.6"
+      )
+      .fromTo(nextLeft, 0.4, { y: "-100%" }, { y: "-5%" }, "-=0.6")
+      .fromTo(nextRight, 0.4, { y: "-100%" }, { y: "5%" }, "-=0.6")
+      .fromTo(nextText, 0.4, { opacity: 0, y: 0 }, { opacity: 1, y: 0 })
+      .set(nextLeft, { clearProps: "all" })
+      .set(nextRight, { clearProps: "all" });
+
+    current = pageNumber;
+  }
+
+  //OPTIONAL
+  document.addEventListener("wheel", throttle(scrollChange, 1500));
+  document.addEventListener("touchmove", throttle(scrollChange, 1500));
+
+  function switchDots(dotNumber) {
+    const activeDot = document.querySelectorAll(".slide")[dotNumber];
+    slides.forEach(slide => {
+      slide.classList.remove("active");
+    });
+    activeDot.classList.add("active");
+  }
+
+  function scrollChange(e) {
+    if (e.deltaY > 0) {
+      scrollSlide += 1;
+    } else {
+      scrollSlide -= 1;
+    }
+
+    if (scrollSlide > 3) {
+      scrollSlide = 0;
+    }
+    if (scrollSlide < 0) {
+      scrollSlide = 3;
+    }
+
+    switchDots(scrollSlide);
+    nextSlide(scrollSlide);
+    console.log(scrollSlide);
+  }
+
+  const menu = document.querySelector(".menu");
+  const menuLines = document.querySelectorAll(".menu line");
+  const navOpen = document.querySelector(".nav-open");
+  const contact = document.querySelector(".contact");
+  const social = document.querySelector(".social");
+  const logo = document.querySelector(".logo");
+
+  const tl = new TimelineMax({ paused: true, reversed: true });
+
+  tl.to(navOpen, 0.5, { y: 0 })
+    .fromTo(contact, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.1")
+    .fromTo(social, 0.5, { opacity: 0, y: 10 }, { opacity: 1, y: 0 }, "-=0.5")
+    .fromTo(logo, 0.2, { color: "black" }, { color: "black" }, "-=1")
+    .fromTo(
+      menuLines,
+      0.2,
+      { stroke: "black" },
+      { stroke: "black" },
+      "-=1"
+    );
+
+  menu.addEventListener("click", () => {
+    tl.reversed() ? tl.play() : tl.reverse();
+  });
+}
+
+function throttle(func, limit) {
+  let inThrottle;
+  return function() {
+    const args = arguments;
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
+
+init();
